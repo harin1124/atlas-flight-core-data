e497b5a4e7eea780c4163234ca7a5521ef1aa12f
harin1124 <lolrin1124@gmail.com>
2026-04-20 14:26:05 +0900

    init

---
+++ src/main/java/org/atlas/flight/coredata/AtlasFlightCoreDataApplication.java
@@ -0,0 +1,13 @@
+package org.atlas.flight.coredata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasFlightCoreDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasFlightCoreDataApplication.class, args);
+	}
+
+}
